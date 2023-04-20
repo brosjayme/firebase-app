d11a2e4cc98ffab050f30d0d27cbd6ac11e3eff1
unknown <brosjayme@gmail.com>
2023-04-20 07:25:47 +0100

    first firebase

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+import { intializeApp } from "https://fir-app-50117-default-rtdb.firebaseio.com/";
+import { getDatebase, ref, push} from "https://fir-app-50117-default-rtdb.firebaseio.com/";
+
+const appSettings ={
+    databaseURL: "https://fir-app-50117-default-rtdb.firebaseio.com/"
+}
+
+const app = intializeApp(appSettings)
+const database = getDatebase(app)
+const shoppingListInDB = ref(database, 'shoppingList')
+
 const inputField = document.getElementById('input-field');
 const addButton = document.getElementById('add-button')
 
 addButton.addEventListener('click', function() {
     let inputValue = inputField.value
+
+    push(shoppingListInDB, inputvalue)
     console.log(inputField.value)
 }) 
