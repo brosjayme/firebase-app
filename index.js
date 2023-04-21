165b9957c6705aa865d70700d1aa48d5b03f2448
unknown <brosjayme@gmail.com>
2023-04-21 07:00:08 +0100

    first firebase

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { intializeApp } from "https://fir-app-50117-default-rtdb.firebaseio.com/";
+import { intializeApp } from "https://www.gstatic.com/-default-rtdb.firebaseio.com/";
 import { getDatebase, ref, push} from "https://fir-app-50117-default-rtdb.firebaseio.com/";
 
-const appSettings ={
+const appSettings = {
     databaseURL: "https://fir-app-50117-default-rtdb.firebaseio.com/"
 }
 
@@ -11,10 +11,17 @@ const shoppingListInDB = ref(database, 'shoppingList')
 
 const inputField = document.getElementById('input-field');
 const addButton = document.getElementById('add-button')
+const shoppingList = document.getElementById('shopping-list')
 
 addButton.addEventListener('click', function() {
     let inputValue = inputField.value
 
-    push(shoppingListInDB, inputvalue)
-    console.log(inputField.value)
-}) 
+    push(shoppingListInDB, inputValue)
+
+    function clearInputField() {
+        inputField.value = ""
+    }
+    function appendItemToShoppingList(itemValue) {
+    shoppingList.innerHTML += `<li>${inputValue}</li>`
+    }
+})    
